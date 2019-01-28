1112f0b6e58fc70b0fb61878aa73fa2049788be7
Judith Lloyd-Jones <judithlloydjones@Judiths-MacBook.local>
2019-01-28 14:17:43 +0000

    CreateUser method working

--- src/chitterAPI.js
+++ src/chitterAPI.js
@@ -1,6 +1,8 @@
 class chitterAPI {
   constructor() {
     this.url = 'https://chitter-backend-api.herokuapp.com';
+    this.sessionKey;
+    this.sessionUserId;
   }
 
   renderPeeps() {
@@ -11,39 +13,48 @@ class chitterAPI {
     })
   }
 
+  signUpUser(handle, password) {
+    var userData = {"user": {"handle":handle, "password":password}};
+    var sessionData = {"session": {"handle":handle, "password":password}};
+
+    this._createUser(userData).then(
+      console.log("Finished signupuser function")
+    )
+  }
+
   _createUser(userData) {
-    $.ajax({
+    console.log("In first AJAX request")
+    var userPromise = $.ajax({
       method: 'POST',
       url: `${this.url}/users`,
       headers: 'Content-Type: application/json',
-      data: userData
+      data: userData,
+      error: function(error) {
+        console.log(error.responseText)
+      },
+      success: this._createSession
     })
+    return userPromise;
   }
 
-  _createSession(sessionData) {
-    $.ajax({
+  _createSession() {
+    console.log("In second AJAX request")
+    var sessionPromise = $.ajax({
       method: 'POST',
       url: `https://chitter-backend-api.herokuapp.com/sessions`, //can't use this as refers to /user url
       headers: 'Content-Type: application/json',
-      data: sessionData
-    }).done(function(sessionInfo) {
-       var sessionUserId = sessionInfo.user_id
-       var sessionKey = sessionInfo.session_key
+      data: sessionData, 
+      // data: {"session": {"handle":this.data.handle, "password":this.data.password}},
+      error: function(error) {
+        console.log('Failed to create session ' + error)
+      },
+      success: function(sessionInfo){
+        console.log('In success for create session. ' + sessionInfo);
+        this.sessionUserId = sessionInfo.user_id;
+        this.sessionKey = sessionInfo.session_key;
+      }
     })
-  }
-
-  signUpUser(handle, password) {
-    let userData = {"user": {"handle":handle, "password":password}};
-    let sessionData = {"session": {"handle":handle, "password":password}};
-    //this methods need to be attached to an instance of class. don't want a class any more
-    _createUser(userData).then(function() {
-      _createSession(sessionData)
-    }).done(function(data){
-      console.log(data)
-    }).fail(function(error) {
-      // can't get this any further .handle doesn't work, comes back as undefined
-      console.log(error.responseText)
-    })
+    return sessionPromise;
   }
 
 }
